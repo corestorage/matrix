ee01df09bda2c27769481f88a79a497a0c8496b0
corestorage <corestorage@yahoo.com>
2025-05-16 02:39:14 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { ThemeProvider } from "next-themes";
 import "styles/globals.css";
 
 export const metadata: Metadata = {
-  title: "Next JS SaaS Starter Template",
-  description: "Next JS SaaS Starter Template",
+  title: "Matrix",
+  description: "Matrix",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
